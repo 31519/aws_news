e4f3ec67dacbdaad36638ebe11174599f1e84e6a
31519 <cosrumut31519@gmail.com>
2021-05-25 23:33:36 +0530

    aws s3 bucket

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,9 +4,6 @@ from django.utils.html import format_html
 # Register your models here.
 
 class UserProfileAdmin(admin.ModelAdmin):
-    def thumbnail(self, object):
-        return format_html('<img src="{}" width="30" style="border-radius:50%;">'.format(object.images.url))
-    thumbnail.short_description = 'Images'
-    list_display = ('thumbnail', 'user', 'address', 'state', 'country')
+    list_display = ['user', 'address', 'state', 'country']
 admin.site.register(Account)
 admin.site.register(UserProfile, UserProfileAdmin)
--- news_portals/settings.py
+++ news_portals/settings.py
@@ -44,9 +44,12 @@ INSTALLED_APPS = [
     'marketing',
     'orders',
     'advertise',
-    
+    'storages',
+
 
 ]
+
+
 
 MAILCHIMP_API_KEY = '6aea3493091a35ef60074e755a1887b8-us6'
 MAILCHIMP_DATA_CENTER = 'us6'
@@ -164,12 +167,32 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'asset')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'asset')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+
+# AWS S3 Static Files Configuration
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'news_portal.media-storages.MediaStorage'
 
 
 
@@ -182,3 +205,5 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+
+
