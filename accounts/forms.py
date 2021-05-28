d9567bdbf70381fa4c1a2bdee36fad3d985464a8
31519 <cosrumut31519@gmail.com>
2021-05-29 01:18:07 +0530

    edit email

--- accounts/forms.py
+++ accounts/forms.py
@@ -26,7 +26,7 @@ class RegistrationForm(forms.ModelForm):
 class UserForm(forms.ModelForm):
     class Meta:
         model = Account
-        fields = ('first_name', 'last_name', 'phone_number', 'images')
+        fields = ('first_name', 'last_name', 'phone_number', 'images', 'email')
 
     def __init__(self, *args, **kwargs):
         super(UserForm, self).__init__(*args, **kwargs)
